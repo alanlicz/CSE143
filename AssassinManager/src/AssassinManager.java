cd203719c36caa0870cb0770c1035ba69a4d683c
alanlicz <alanltycz@icloud.com>
2020-04-22 15:18:18 -0700

    fix kill method

--- AssassinManager/src/AssassinManager.java
+++ AssassinManager/src/AssassinManager.java
@@ -24,17 +24,17 @@ public class AssassinManager {
     public void printKillRing(){
         AssassinNode current = killRing;
         while (current.next != null){
-            System.out.println(current.name + " is stalking " + current.next.name);
+            System.out.println("    " + current.name + " is stalking " + current.next.name);
             current = current.next;
         }
-        System.out.println(current.name + " is stalking " + killRing.name);
+        System.out.println("    " + current.name + " is stalking " + killRing.name);
     }
 
     public void printGraveyard(){
         AssassinNode current = graveyard;
         String output = "";
         while(current != null) {
-            output += current.name + " was killed by " + current.killer + "\n";
+            output += "    " + current.name + " was killed by " + current.killer + "\n";
             current = current.next;
         }
         if (!output.equals("")){
@@ -85,23 +85,29 @@ public class AssassinManager {
     }
 
     public void kill(String name){
-        name = name.toLowerCase();
+        AssassinNode currentAssassin = killRing;
         AssassinNode currentGrave = graveyard;
-        AssassinNode current = killRing;
-        AssassinNode temp = killRing;
-        if (killRing.name.equalsIgnoreCase(name) && graveyard == null){
-            graveyard = killRing;
-            killRing = killRing.next;
-            while (temp.next != null){
-                temp = temp.next;
+        if (!killRingContains(name)) {
+            throw new IllegalArgumentException();
+        } else if (gameOver()) {
+            throw new IllegalStateException();
+        }
+        if (currentAssassin.name.equalsIgnoreCase(name)) {
+            graveyard = currentAssassin;
+            while (currentAssassin.next != null){
+                currentAssassin = currentAssassin.next;
             }
-            currentGrave.killer = temp.name;
-        }
-        if (killRing.name.equalsIgnoreCase(name)){
-            currentGrave.next = killRing;
             killRing = killRing.next;
+        } else {
+            while (!currentAssassin.next.name.equalsIgnoreCase(name)) {
+                currentAssassin = currentAssassin.next;
+            }
+            graveyard = currentAssassin.next;
+            currentAssassin.next = currentAssassin.next.next;
         }
-
+        graveyard.next = currentGrave;
+        graveyard.killer = currentAssassin.name;
     }
+
 
 }
